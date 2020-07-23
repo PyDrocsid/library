c879ed04b3aafaae47d0bee447ae17262df5fae5
Defelo <elodef42@gmail.com>
2020-07-23 23:38:51 +0200

    Added python package

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+from os import environ
+
+from setuptools import find_packages
+
+setup(
+    name="PyDrocsid",
+    version=environ["VERSION"],
+    url="https://github.com/Defelo/PyDrocsid",
+    author="Defelo",
+    author_email="elodef42@gmail.com",
+    description="Python Discord Bot Framework based on Discord.py",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=open("requirements.txt").read().splitlines(),
+)
