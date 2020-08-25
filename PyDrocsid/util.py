7bbd92761a3e8579ab0f7da0703e311a220f80cf
luwol03 <luwol03@gmail.com>
2020-08-25 16:32:11 +0200

    Added send_editable_log function

--- PyDrocsid/util.py
+++ PyDrocsid/util.py
@@ -124,3 +124,29 @@ async def read_complete_message(message: Message) -> Tuple[str, List[File], Opti
         embed = None
 
     return message.content, [await attachment_to_file(attachment) for attachment in message.attachments], embed
+
+
+async def send_editable_log(channel: Messageable, title: str, name: str, value: str,
+                            colour: Optional[int] = 0x008080, inline: Optional[bool] = False,
+                            force_resend: Optional[bool] = False, force_new_embed: Optional[bool] = False):
+    messages = await channel.history(limit=1).flatten()
+    if messages and messages[0].embeds and not force_new_embed:
+        embed = messages[0].embeds[0]
+        if embed.title == title:
+            if embed.fields and embed.fields[-1].name == name:
+                embed.set_field_at(index=-1, name=name, value=value, inline=inline)
+            elif len(embed.fields) < 25:
+                embed.add_field(name=name, value=value, inline=inline)
+            else:
+                force_new_embed = True
+            if not force_new_embed:
+                if force_resend:
+                    await messages[0].delete()
+                    await channel.send(embed=embed)
+                    return
+                await messages[0].edit(embed=embed)
+                return
+
+    embed = Embed(title=title, colour=colour)
+    embed.add_field(name=name, value=value, inline=inline)
+    await channel.send(embed=embed)
