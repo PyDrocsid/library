7ef734c59c894efd18ede39d49806ff1ceb27732
Paul-Brauckmann <paul.brauckmann@outlook.com>
2021-02-24 16:34:38 +0100

    add logging

---
+++ PyDrocsid/logging.py
@@ -0,0 +1,35 @@
+import logging
+import sentry_sdk
+
+logging_handler = logging.StreamHandler()
+logging_formatter = logging.Formatter('{asctime} - {levelname} - {name} - {message}', style='{')
+
+
+class Message:
+    def __init__(self, fmt: str, /, args):
+        self._fmt = fmt
+        self._args = args
+
+    def __str__(self):
+        return self._fmt.format(*self._args)
+
+
+class LoggingAdapter(logging.LoggerAdapter):
+    def __init__(self, logger, extra=None):
+        super().__init__(logger, extra or {})
+
+    def log(self, level: int, msg: str, /, *args, error=None, **kwargs):
+        if self.isEnabledFor(level):
+            msg, kwargs = self.process(msg, kwargs)
+            self.logger._log(level, Message(msg, args), (), {})
+
+        if level >= logging.WARNING and error:
+            sentry_sdk.capture_exception(error)
+
+
+def get_logger(name, handler=logging_handler, formatter=logging_formatter, level=logging.INFO):
+    logger = LoggingAdapter(logging.getLogger(name))
+    handler.setFormatter(formatter)
+    logger.logger.addHandler(handler)
+    logger.setLevel(level)
+    return logger
