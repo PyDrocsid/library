ba2e4bc5b141e05c708eff3ceac524fe339b6977
Defelo <elodef42@gmail.com>
2021-05-16 21:27:50 +0200

    Added docstrings to async_thread.py

--- PyDrocsid/async_thread.py
+++ PyDrocsid/async_thread.py
@@ -1,23 +1,30 @@
-import asyncio
 import threading
-from asyncio import Semaphore, Lock
+from asyncio import Semaphore, Lock, Event, get_running_loop, AbstractEventLoop, gather, create_task
 
 from functools import partial, update_wrapper, wraps
+from typing import Callable, TypeVar, Optional, Coroutine
+
+T = TypeVar("T")
 
 
 class Thread(threading.Thread):
-    def __init__(self, func, loop):
+    def __init__(self, func: Callable[..., T], loop: AbstractEventLoop):
         super().__init__()
-        self._return = None
-        self._func = func
-        self._event = asyncio.Event()
-        self._loop = loop
+
+        self._return: Optional[T] = None
+        self._func: Callable[..., T] = func
+        self._event = Event()
+        self._loop: AbstractEventLoop = loop
 
     async def wait(self):
+        """Wait for function to finish and return the result."""
+
         await self._event.wait()
         return self._return
 
     def run(self):
+        """Run the function and set the event after completion."""
+
         try:
             self._return = True, self._func()
         except Exception as e:  # skipcq: PYL-W0703
@@ -26,7 +33,16 @@ class Thread(threading.Thread):
 
 
 async def run_in_thread(func, *args, **kwargs):
-    thread = Thread(partial(func, *args, **kwargs), asyncio.get_running_loop())
+    """
+    Run a synchronous function asynchronously using threading.
+
+    :param func: the synchronous function
+    :param args: positional arguments to pass to the function
+    :param kwargs: keyword arguments to pass to the function
+    :return: the return value of func(*args, **kwargs)
+    """
+
+    thread = Thread(partial(func, *args, **kwargs), get_running_loop())
     thread.start()
     ok, result = await thread.wait()
     if not ok:
@@ -35,17 +51,27 @@ async def run_in_thread(func, *args, **kwargs):
     return result
 
 
-async def semaphore_gather(n, *tasks):
+async def semaphore_gather(n: int, *tasks: Coroutine) -> list:
+    """
+    Like asyncio.gather, but limited to n concurrent tasks.
+
+    :param n: the maximum number of concurrent tasks
+    :param tasks: the coroutines to run
+    :return: a list containing the results of all coroutines
+    """
+
     semaphore = Semaphore(n)
 
     async def inner(t):
         async with semaphore:
             return await t
 
-    return await asyncio.gather(*map(inner, tasks))
+    return list(await gather(*map(inner, tasks)))
 
 
 class LockDeco:
+    """Decorator for synchronisation of async functions"""
+
     def __init__(self, func):
         self.lock = Lock()
         self.func = func
@@ -57,8 +83,13 @@ class LockDeco:
 
 
 def run_as_task(func):
+    """
+    Decorator for async functions.
+    Instead of calling the decorated function directly, this will create a task for it and return immediately.
+    """
+
     @wraps(func)
     async def inner(*args, **kwargs):
-        asyncio.create_task(func(*args, **kwargs))
+        create_task(func(*args, **kwargs))
 
     return inner
