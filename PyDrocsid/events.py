ae38d7ea23d6e7be5a11baef4cb4e7902e50cd62
Defelo <elodef42@gmail.com>
2021-05-17 12:50:45 +0200

    Added docstrings to events.py

--- PyDrocsid/events.py
+++ PyDrocsid/events.py
@@ -1,7 +1,7 @@
 import re
 from datetime import datetime
 from functools import partial
-from typing import Optional, Union, Tuple, Callable
+from typing import Optional, Union, Tuple, Callable, Awaitable
 
 from discord import (
     Member,
@@ -29,6 +29,8 @@ from PyDrocsid.multilock import MultiLock
 
 
 class StopEventHandling(Exception):
+    """Raise this exception to prevent remaining event handlers from handling the current event."""
+
     pass
 
 
@@ -36,14 +38,25 @@ async def extract_from_raw_reaction_event(
     bot: Bot,
     event: RawReactionActionEvent,
 ) -> Optional[Tuple[Message, PartialEmoji, Union[User, Member]]]:
+    """
+    Extract message, emoji and user from any RawReactionActionEvent.
+
+    :param bot: the bot instance
+    :param event: the RawReactionActionEvent
+    :return: a (message, emoji, user) tuple or None, if any of these entities does not exist
+    """
+
     channel: Optional[Messageable] = bot.get_channel(event.channel_id)
     if channel is None:
         return None
 
     if isinstance(channel, TextChannel):
+        # guild channel
         user: Member = channel.guild.get_member(event.user_id)
     else:
+        # direct message
         user: User = bot.get_user(event.user_id)
+
     if user is None:
         return None
 
@@ -56,6 +69,12 @@ async def extract_from_raw_reaction_event(
 
 
 class Events:
+    """
+    Collection of all registrable event handlers
+    For a more detailed documentation on these events, please refer to
+    https://discordpy.readthedocs.io/en/latest/ext/commands/api.html#event-reference
+    """
+
     @staticmethod
     async def on_ready(_):
         await call_event_handlers("ready")
@@ -73,13 +92,18 @@ class Events:
         if not await call_event_handlers("message", message, identifier=message.id):
             return
 
-        match = re.match(r"^<@[&!]?(\d+)>$", message.content.strip())
-        if match:
+        # detect whether the message contains just a mention of the bot
+        # and call the bot_ping event
+        if match := re.match(r"^<@[&!]?(\d+)>$", message.content.strip()):
             mentions = {bot.user.id}
+
+            # find managed role of this bot
             if message.guild is not None:
                 for role in message.guild.me.roles:  # type: Role
                     if role.managed:
                         mentions.add(role.id)
+
+            # call bot_ping if bot has been mentioned
             if int(match.group(1)) in mentions:
                 await call_event_handlers("bot_ping", message, identifier=message.id)
                 return
@@ -89,6 +113,8 @@ class Events:
     @staticmethod
     async def on_message_delete(bot: Bot, message: Message):
         await call_event_handlers("message_delete", message, identifier=message.id)
+
+        # delete bot responses if message contained a command
         await handle_delete(bot, message.channel.id, message.id)
 
     @staticmethod
@@ -97,6 +123,8 @@ class Events:
             return
 
         await call_event_handlers("raw_message_delete", event, identifier=event.message_id)
+
+        # delete bot responses if message contained a command
         await handle_delete(bot, event.channel_id, event.message_id)
 
     @staticmethod
@@ -114,9 +142,12 @@ class Events:
         prepared = []
 
         async def prepare():
+            """Extract channel and message from event"""
+
             channel: Optional[Messageable] = bot.get_channel(event.channel_id)
             if channel is None:
                 return
+
             try:
                 message: Message = await channel.fetch_message(event.message_id)
             except NotFound:
@@ -128,6 +159,8 @@ class Events:
         await call_event_handlers("raw_message_edit", identifier=event.message_id, prepare=prepare)
 
         if prepared:
+            # delete bot responses if old message contained a command
+            # and execute command if new message contains one
             await handle_edit(bot, prepared[0])
 
     @staticmethod
@@ -147,9 +180,12 @@ class Events:
     @staticmethod
     async def on_raw_reaction_clear(bot: Bot, event: RawReactionClearEvent):
         async def prepare():
+            """Extract message from event."""
+
             channel: Optional[Messageable] = bot.get_channel(event.channel_id)
             if channel is None:
                 return None
+
             try:
                 return [await channel.fetch_message(event.message_id)]
             except NotFound:
@@ -160,9 +196,12 @@ class Events:
     @staticmethod
     async def on_raw_reaction_clear_emoji(bot: Bot, event: RawReactionClearEmojiEvent):
         async def prepare():
+            """Extract message and emoji from event."""
+
             channel: Optional[Messageable] = bot.get_channel(event.channel_id)
             if channel is None:
                 return None
+
             try:
                 return await channel.fetch_message(event.message_id), event.emoji
             except NotFound:
@@ -180,9 +219,11 @@ class Events:
 
     @staticmethod
     async def on_member_update(_, before: Member, after: Member):
+        # check if nickname has been updated
         if before.nick != after.nick:
             await call_event_handlers("member_nick_update", before, after, identifier=before.id)
 
+        # check if roles have been added or removed
         roles_before = set(before.roles)
         roles_after = set(after.roles)
         for role in roles_before:
@@ -221,12 +262,13 @@ class Events:
         await call_event_handlers("command_error", ctx, error, identifier=ctx.message.id)
 
 
-event_handlers = {}
-cog_instances = {}
+event_handlers: dict[str, list[Callable[..., Awaitable]]] = {}
 handler_lock = MultiLock()
 
 
-def listener(func: Callable):
+def listener(func: Callable[..., Awaitable]):
+    """Decorator for registering a new event handler."""
+
     name: str = func.__name__
     if not name.startswith("on_"):
         raise Exception("Invalid listener name")
@@ -235,6 +277,19 @@ def listener(func: Callable):
 
 
 async def call_event_handlers(event: str, *args, identifier=None, prepare=None) -> bool:
+    """
+    Call handlers for a given event.
+
+    :param event: the name of the event
+    :param args: positional arguments to pass to the event handler
+    :param identifier: synchronisation identifier of this event (two different events with the same
+                       identifier cannot be handled simultaneously)
+    :param prepare: async function that is called before handling this event. If this function returns
+                    None, the event is ignored. Otherwise the iterable this function must return
+                    is passed to the event handlers as a list of positional arguments.
+    :return: True if all handlers for this event have been called without raising StopEventHandling, otherwise False
+    """
+
     async with handler_lock[(event, identifier) if identifier is not None else None]:
         if prepare is not None:
             args = await prepare()
@@ -251,9 +306,12 @@ async def call_event_handlers(event: str, *args, identifier=None, prepare=None) 
 
 
 def register_events(bot: Bot):
+    """Register all events defined in Events class"""
+
     for e in dir(Events):
         func = getattr(Events, e)
         if e.startswith("on_") and callable(func):
+            # always wrap event handlers in database sessions and pass the bot instance as first argument
             handler = partial(db_wrapper(func), bot)
             handler.__name__ = e
             bot.event(handler)
