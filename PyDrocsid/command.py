c81901eebc7837f4aeb224116ed68e79a1b24029
Defelo <elodef42@gmail.com>
2021-05-21 14:46:06 +0200

    Added confirm function

--- PyDrocsid/command.py
+++ PyDrocsid/command.py
@@ -1,13 +1,18 @@
+import asyncio
+from contextlib import asynccontextmanager
+from copy import deepcopy
 from typing import Union
 
-from discord import User, Member, Embed, Message
+from discord import User, Member, Embed, Message, Forbidden
 from discord.abc import Messageable
 from discord.ext.commands import Command, Context, CommandError
 
+from PyDrocsid.async_thread import gather_any
 from PyDrocsid.command_edit import link_response
 from PyDrocsid.environment import REPLY, MENTION_AUTHOR
 from PyDrocsid.material_colors import MaterialColors
 from PyDrocsid.translations import t
+from library.PyDrocsid.emojis import name_to_emoji
 
 t = t.g
 
@@ -74,3 +79,37 @@ async def reply(ctx: Union[Context, Message, Messageable], *args, no_reply: bool
         await link_response(ctx, msg)
 
     return msg
+
+
+@asynccontextmanager
+async def confirm(ctx: Context, embed: Embed, timeout: int = 300):
+    _embed: dict = deepcopy(embed.to_dict())
+    message: Message = await reply(ctx, embed=embed)
+    await message.add_reaction(yes := name_to_emoji["white_check_mark"])
+    await message.add_reaction(no := name_to_emoji["x"])
+
+    def check(r, u):
+        return r.message == message and u == ctx.author and str(r) in [yes, no]
+
+    i, result = await gather_any(
+        ctx.bot.wait_for("reaction_add", check=check),
+        ctx.bot.wait_for("message_delete", check=lambda msg: msg == message),
+        asyncio.sleep(timeout),
+    )
+
+    if i == 0:
+        reaction, _ = result
+        yield str(reaction) == yes
+    else:
+        yield False
+    if i == 1:
+        return
+
+    if embed.to_dict() != _embed:
+        await message.edit(embed=embed)
+
+    try:
+        await message.clear_reactions()
+    except Forbidden:
+        await message.remove_reaction(yes, ctx.bot.user)
+        await message.remove_reaction(no, ctx.bot.user)
