19e2c011032a983baf4b199ad026a0436e9f8f84
Defelo <elodef42@gmail.com>
2022-04-01 17:21:15 +0200

    Fixed converter.py

--- PyDrocsid/converter.py
+++ PyDrocsid/converter.py
@@ -1,7 +1,7 @@
 import re
-from typing import cast
 
-from discord import PartialEmoji, Member, User, Guild, NotFound, HTTPException, Colour
+from discord import PartialEmoji, Member, User, Guild, NotFound, HTTPException
+from discord.ext.commands import Bot
 from discord.ext.commands.context import Context
 from discord.ext.commands.converter import PartialEmojiConverter, ColorConverter, Converter
 from discord.ext.commands.errors import BadArgument
@@ -12,12 +12,12 @@ from PyDrocsid.translations import t
 t = t.g
 
 
-class EmojiConverter(PartialEmojiConverter):  # type: ignore
+class EmojiConverter(PartialEmojiConverter):
     """Emoji converter which also supports unicode emojis."""
 
-    async def convert(self, ctx: Context, argument: str) -> PartialEmoji:
+    async def convert(self, ctx: Context[Bot], argument: str) -> PartialEmoji:
         try:
-            return cast(PartialEmoji, await super().convert(ctx, argument))
+            return await super().convert(ctx, argument)
         except BadArgument:
             pass
 
@@ -28,12 +28,12 @@ class EmojiConverter(PartialEmojiConverter):  # type: ignore
         return PartialEmoji.with_state(connection, animated=False, name=argument, id=None)
 
 
-class Color(ColorConverter):  # type: ignore
+class Color(Converter):  # type: ignore
     """Color converter which also supports hex codes."""
 
-    async def convert(self, ctx: Context, argument: str) -> int:
+    async def convert(self, ctx: Context[Bot], argument: str) -> int:
         try:
-            return cast(Colour, (await super().convert(ctx, argument))).value
+            return (await ColorConverter().convert(ctx, argument)).value
         except BadArgument:
             pass
 
@@ -45,7 +45,7 @@ class Color(ColorConverter):  # type: ignore
 class UserMemberConverter(Converter):  # type: ignore
     """Return a member or user object depending on whether the user is currently a guild member."""
 
-    async def convert(self, ctx: Context, argument: str) -> User | Member:
+    async def convert(self, ctx: Context[Bot], argument: str) -> User | Member:
         guild: Guild = ctx.bot.guilds[0]
 
         if not (match := re.match(r"^(<@!?)?([0-9]{15,20})(?(1)>)$", argument)):
@@ -56,6 +56,6 @@ class UserMemberConverter(Converter):  # type: ignore
         if member := guild.get_member(user_id):
             return member
         try:
-            return cast(User, await ctx.bot.fetch_user(user_id))
+            return await ctx.bot.fetch_user(user_id)
         except (NotFound, HTTPException):
             raise BadArgument(t.user_not_found)
