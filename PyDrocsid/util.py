1a89cc23de29e32b7270e7d1178261aedb2f1da5
LoC <loc@locrealloc.de>
2022-07-12 11:09:03 +0200

    Enhanced docstring as requested by the reviewers
    Co-authored-by: Tristan <45330667+Tristan-H11@users.noreply.github.com>

--- PyDrocsid/util.py
+++ PyDrocsid/util.py
@@ -238,7 +238,7 @@ def check_message_send_permissions(
 
 def time_to_units(minutes: int | float) -> str:
     """
-    Util function to split minutes back into different time units
+    Util function to convert minutes into different time units
     """
     _keys = ("years", "months", "days", "hours", "minutes")
 
